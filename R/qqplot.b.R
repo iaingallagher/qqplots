1f5516a38234aa1cf60e346fdea77f6672514a59
iaingallagher <iaingallagher@gmail.com>
2017-09-24 10:58:04 +0100

    added qqplot.b.R with data, variable and plot data collection defined

---
+++ R/qqplot.b.R
@@ -0,0 +1,19 @@
+
+# This file is a generated template, your changes will not be overwritten
+
+qqplotClass <- if (requireNamespace('jmvcore')) R6::R6Class(
+    "qqplotClass",
+    inherit = qqplotBase,
+    private = list(
+        .run = function() {
+
+            # `self$data` contains the data
+            # `self$options` contains the options
+            # `self$results` contains the results object (to populate)
+          
+          qqvar <- self$options$data[,self$options$var]
+          qqdata <- qqnorm(qqvar, plot.it=FALSE)
+          plotData <- data.frame(x=qqdata$x, y=qqdata$y)
+
+        })
+)
